d70766ed25e59f4028cce9504209bd1643dae427
KMN <malaojoijoi@gmail.com>
2022-02-02 19:37:04 +0700

    / first step

---
+++ src/main/java/com/kaewmanee/lab9/App.java
@@ -0,0 +1,15 @@
+package com.kaewmanee.lab9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Map map = new Map();
+        map.print();
+        System.out.println( map);
+    }
+}
---
+++ src/main/java/com/kaewmanee/lab9/Map.java
@@ -0,0 +1,24 @@
+package com.kaewmanee.lab9;
+
+public class Map {
+    private int width;
+    private int height;
+    public Map(int width,int height) {
+        this.width = width;
+        this.height = height;
+    }
+    public Map(){
+        this(10,10);
+    }
+    public void print(){
+        for(int i = 0; i <width; i++){
+            for(int j = 0; j <height; j++){
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
+    public String toString(){
+        return "Map ("+this.width+","+this.height+")";
+    }
+}
