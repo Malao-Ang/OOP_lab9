016f4717fb25c1bf97a9f77a81e9eb43dd9bd578
KMN <malaojoijoi@gmail.com>
2022-02-02 23:31:17 +0700

    left right up down case process finished

--- src/main/java/com/kaewmanee/lab9/App.java
+++ src/main/java/com/kaewmanee/lab9/App.java
@@ -1,25 +1,59 @@
 package com.kaewmanee.lab9;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static Map map = new Map(15,15);
+    static Robot robot = new Robot(map,'A',10,10);
+    static Tree tree1 = new Tree(map,5,5);
+    static Tree tree2 = new Tree(map,5,6);
+    static Tree tree3 = new Tree(map,6,5);
+    static Tree tree4 = new Tree(map,7,5);
+    static Tree tree5 = new Tree(map,5,7);
+    static Tree tree6 = new Tree(map,6,7);
+    static Tree tree7 = new Tree(map,11,9);
+    static Tree tree8 = new Tree(map,11,10);
+    static Tree tree9 = new Tree(map,11,12);
+    static Tree tree10 = new Tree(map,12,10);
+    static Scanner sc = new Scanner(System.in);
+    
+    public static String input(){
+        return sc.next();
+    }
+    public static void process(String command){
+        switch (command) {
+            case "w":
+                robot.up();
+                break;
+
+            case "s":
+                robot.down();
+                break;
+
+            case "a":
+                robot.left();
+                break;
+
+            case "d":
+                robot.right();
+                break;
+            case "q":
+                System.exit(0);
+                break;
+
+            default:
+                break;
+        
+        }
+    }
     public static void main( String[] args )
     {
-        Map map = new Map(15,15);
-        Robot robot = new Robot(map,'A',10,10);
-        Tree tree1 = new Tree(map,5,5);
-        Tree tree2 = new Tree(map,5,6);
-        Tree tree3 = new Tree(map,6,5);
-        Tree tree4 = new Tree(map,7,5);
-        Tree tree5 = new Tree(map,5,7);
-        Tree tree6 = new Tree(map,6,7);
-        Tree tree7 = new Tree(map,11,9);
-        Tree tree8 = new Tree(map,11,10);
-        Tree tree9 = new Tree(map,11,12);
-        Tree tree10 = new Tree(map,12,10);
+        
         map.add(tree1);
         map.add(tree2);
         map.add(tree3);
@@ -31,10 +65,13 @@ public class App
         map.add(tree9);
         map.add(tree10);
         map.add(robot);
-        robot.setXY(7,5);
-        robot.setXY(15,15);
-        map.print();
-        map.printUnit();
+        
+
+        while(true) {
+            map.print();
+            String command = input();
+            process(command);
+        }
         
     }
 }
--- src/main/java/com/kaewmanee/lab9/Robot.java
+++ src/main/java/com/kaewmanee/lab9/Robot.java
@@ -6,5 +6,28 @@ public class Robot extends Unit{
         super(map, symbol, x, y, false);
         
     }
+    public void up(){
+        int y = this.getY();
+        y--;
+        this.setY(y);
+    }
+    public void down(){
+        int y = this.getY();
+        y++;
+        this.setY(y);
+
+    }
+    public void left(){
+        int  x = this.getX();
+        x--;
+        this.setX(x);
+
+    }
+    public void right(){
+        int x = this.getX();
+        x++;
+        this.setX(x);
+
+    }
     
 }
